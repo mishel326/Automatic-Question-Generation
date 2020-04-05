e0b6638916a9e55432fe97df9cf46f83782cd8a7
mishel326 <misheel@ac.sce.ac.il>
2020-04-05 14:48:55 +0300

    work with stanford

--- code.py
+++ code.py
@@ -8,17 +8,23 @@ Created on Thu Jan 30 23:59:40 2020
 import json
 import re
 import operator
-#import numpy as np
+import numpy as np
+import logging
 from functools import reduce
 from stanfordcorenlp import StanfordCoreNLP
 
-nlp = StanfordCoreNLP(r'stanford-corenlp-full-2018-02-27')
+#nlp = StanfordCoreNLP(r'stanford-corenlp-full-2018-02-27')
+
+print("pass")
+
+paragraph_pad_size = [0]
+question_pad_size = [0]
 
 def clean_text(text):
     '''Clean text by removing unnecessary characters and altering the format of words.'''
 
     text = text.lower()
-    
+    text = re.sub(r"([0-9]+(\.[0-9]+)?)",r" \1 ", text)
     text = re.sub(r"i'm", "i am", text)
     text = re.sub(r"\'s", " is", text)
     text = re.sub(r"\'ll", " will", text)
@@ -26,6 +32,7 @@ def clean_text(text):
     text = re.sub(r"\'re", " are", text)
     text = re.sub(r"\'d", " would", text)
     text = re.sub(r"\'re", " are", text)
+    text = re.sub(r"\'em", " are", text)
     text = re.sub(r"won't", "will not", text)
     text = re.sub(r"can't", "can not", text)
     text = re.sub(r"cannot", "can not", text)
@@ -33,11 +40,13 @@ def clean_text(text):
     text = re.sub(r"n'", "ng", text)
     text = re.sub(r"'bout", "about", text)
     text = re.sub(r"'til", "until", text)
-    text = re.sub("[^a-z0-9 $%-]+", '', text.replace('.',' ').replace(',',' ').replace(':',' ').replace('-',' - ').replace('$',' $ ').replace('%',' % '))
+    #text = re.sub(r'[^\w\s]','',text)
+    #text = re.sub("[^a-z0-9 $%-]+", '', text.replace('.',' ').replace(',',' ').replace(':',' ').replace('-',' - ').replace('$',' $ ').replace('%',' % ').replace('/',' / '))
     return text
+                
 
 def read_and_clean(paragraphs,question,file_name):
-    with open(file_name) as file:
+    with open(file_name,encoding='utf-8') as file:
         data = json.loads(file.read())
         for i in data:
             temp_data = i['paragraphs']
@@ -52,11 +61,26 @@ def read_and_clean(paragraphs,question,file_name):
 def func(paragrphs,questions):
     new_paragrphs = []
     new_questions = []
-    for i in range(len(paragrphs)):
-        for j in range(len(questions[i])):
-            new_paragrphs.append(paragrphs[i])
+    for paragrph in range(len(paragrphs)):
+        for question in range(len(questions[paragrph])):
+            new_paragrphs.append(paragrphs[paragrph])
     new_questions = reduce(operator.concat, questions)
     return new_paragrphs,new_questions
+
+def stanford(_list):
+    with StanfordCoreNLP(r'stanford-corenlp-full-2018-02-27', quiet=False, logging_level=logging.DEBUG) as snlp:
+        new_list = []
+        for sent in _list :
+            sentence = []
+            stanford_obj = snlp.annotate(sent, properties={'annotators': "ssplit",'outputFormat': 'json', 'timeout':'5000000'})
+            stanford_obj = json.loads(stanford_obj)
+            for x in stanford_obj['sentences']:
+                for j in x['tokens']:
+                    sentence.append( j['word'] ) 
+            new_list.append(sentence)
+    return new_list
+                    
+    
       
 #train_paragraphs = []
 #train_questions = []
@@ -64,46 +88,69 @@ def func(paragrphs,questions):
 #test_questions = []
 dev_paragraphs = []
 dev_questions = []
-#read_and_clean(train_paragraphs, train_questions, desktop_path + "\train.json")
-#read_and_clean(test_paragraphs, test_questions, desktop_path + "\test.json")
+#read_and_clean(train_paragraphs, train_questions,r"dataset\train.json")
+#read_and_clean(test_paragraphs, test_questions, r"dataset\test.json")
 read_and_clean(dev_paragraphs, dev_questions, "dataset\dev.json")
 
-dev_paragraphs,dev_questions = func(dev_paragraphs,dev_questions)
+dev_paragraphs, dev_questions = func(dev_paragraphs,dev_questions)
+dev_paragraphs, dev_questions = stanford(dev_paragraphs),stanford(dev_questions)
+#dev_paragraphs, dev_questions = func(dev_paragraphs,dev_questions)
+#train_paragraphs, train_questions = func(train_paragraphs, train_questions)
+#test_paragraphs, test_questions = func(test_paragraphs, test_questions)
 
 dictionry = {"pad":0,"<BOS>":1,"<EOS>":2}  
- 
+
 def build_vocab(paragraphs):
-    for paragraph in paragraphs:
-        if paragraph not in dictionry:
-            dictionry[paragraph] = len(dictionry)
-    
+    for i in paragraphs:
+        for j in i:
+            if j not in dictionry:
+                dictionry[j] = len(dictionry)
+     
 def build_vocab_list(list_words):
     for paragraph in list_words:
-        build_vocab(paragraph.split())
-            
-build_vocab_list(dev_paragraphs)
-build_vocab_list(dev_questions)
+        build_vocab(paragraph)
+             
+build_vocab(dev_paragraphs)
+build_vocab(dev_questions)
+#build_vocab(train_paragraphs)
+#build_vocab(train_questions)
+#build_vocab(test_paragraphs)
+#build_vocab(test_questions)
+
+def list_to_index(_list):
+    for paragraph in _list:
+        _list[_list.index(paragraph)] = sentence_to_index(paragraph)
 
 def sentence_to_index(sentences):
     for sentence in range(len(sentences)):
         sentences[sentence] = dictionry[sentences[sentence]]
-    return sentences
+    return sentences 
 
-def list_to_index(_list):
+def padding(_list,pad):
+    pad[0] = reduce(lambda x,y:x if x > len(y) else len(y),_list,pad[0])
     for i in range(len(_list)):
-        _list[i] = sentence_to_index(_list[i].split())
+        _list[i] = _list[i]+[0]*(pad[0] - len(_list[i]))
 
-def padding(_list):
-    pad_size = reduce(lambda x,y:x if x > len(y) else len(y),_list,0)
-    for i in range(len(_list)):
-        _list[i] = _list[i]+[0]*(pad_size - len(_list[i]))
-
-
+#list_to_index(train_paragraphs)
+#list_to_index(train_questions) 
+#list_to_index(test_paragraphs)
+#list_to_index(test_questions)      
 list_to_index(dev_paragraphs)
 list_to_index(dev_questions)
+ 
+#padding(train_paragraphs,paragraph_pad_size)
+#padding(train_questions,question_pad_size)
+#padding(test_paragraphs,paragraph_pad_size)
+#padding(test_questions,question_pad_size)
 
-padding(dev_paragraphs)
-padding(dev_questions)
+padding(dev_paragraphs,paragraph_pad_size)
+padding(dev_questions,question_pad_size)
 
+dev_paragraphs = np.array(dev_paragraphs)
+dev_questions = np.array(dev_questions)
 
-nlp.close()
+#train_paragraphs = np.array(train_paragraphs)
+#train_questions = np.array(train_questions)
+#test_paragraphs = np.array(test_paragraphs)
+#test_questions = np.array(test_questions)
+
